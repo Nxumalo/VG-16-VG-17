20c7b19524c16c50148286a47d40646d7e4e740d
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-04-17 18:55:14 +0200

    Assignment Operator

---
+++ Statement.java
@@ -0,0 +1,24 @@
+public class Statement{
+ public static void main(String [] args)
+   int i,j;
+   double sale;
+   char first;
+   String str;
+  
+   i = 4;
+   System.out.println(“i = “ +i);
+ 
+  j = 4*5 - 11;
+  System.out.println(“j = “+j);
+ 
+  sale = 0.02 * 1000;
+  System.out.println(“sale = “ +sale);
+
+  first = ‘D’;
+  System.out.println(“first = “ +first);
+
+  str = “It is a sunny day.”;
+  System.out.println(“str = “ +str);
+}
+}
+
