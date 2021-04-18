fe866f4934f5b358150a2a289f2f6326563cef3b
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-04-18 15:09:01 +0200

    New line escape sequence

---
+++ Escapecharacter.java
@@ -0,0 +1,20 @@
+public class Escapecharacter{
+ public static void main(String [] args){
+   
+   int a = 65;
+   int b = 78;
+
+   System.out.println(29/4);
+   System.out.println(3.0/2);
+   System.out.print(“Hello there.\n”);
+   System.out.println(7);
+   System.out.println(3+5);
+   System.out.print(“3 + 5”);
+   System.out.println();
+   System.out.println(2-28/7);
+   System.out.println(“a”);
+   System.out.println(“a = “ +a);
+   System.out.println(“b = “ +b);
+   System.out.println(“a + b = “ + (a + ));
+ } 
+}
