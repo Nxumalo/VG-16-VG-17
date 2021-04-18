a83bc07117b3a4ba0352216bf29c4efb0e8afaa8
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-04-18 13:16:24 +0200

    Input Numeric Statement

---
+++ Inputstatement.java
@@ -0,0 +1,16 @@
+import Java.util.*;
+public class Inputstatement{
+ static Scanner console = new Scanner(System.in);
+ public static void main(String[] args){
+   
+   int feet;
+   int inches;
+
+   System.out.println(“Enter two integers seperated by spaces.”);
+   feet = console.nextInt();
+   inched = console.nextInt();
+   
+   System.out.println(“Feet = “ + feet);
+   System.out.println(“Inches = “ +inches);
+ }
+}
