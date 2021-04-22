9ea4e0e74e8e211733e5208e27a6a13d963092c7
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-04-22 15:24:41 +0200

    Strangers Things

---
+++ Strange.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Strange{
+  
+  static Scanner console = new Scanner(System.in);
+  
+  public static void main(String [] arg){
+    
+    int x,y;
+    String name;
+    
+    x = console.nextInt();
+    name = console.nextLine();
+    y = console.nextInt();
+  }
+}
