dc8dc14d1672338b6eea8d14a55dd567f6f89999
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-04-22 15:11:51 +0200

    Program with an Error

---
+++ ProgWithErrorsB.java
@@ -0,0 +1,16 @@
+public class ProgWithErrorsB{
+  
+  static final char = BLANK = '';
+  static final int ONE = 1;
+  
+  public static void main(String [] args){
+    
+    int a,b,c;
+    
+    a = ONE + 5;
+    b = a +BLANK;
+    c := a + ONE * 2;
+    a + c = b;
+    System.out.println(" a = " +a+ ", b = " +b+ ", c = "  +c);
+  }
+}
