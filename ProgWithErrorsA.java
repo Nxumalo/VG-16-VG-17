1a068fc57eac8d747247d9d8753ee42bb528e47c
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-04-22 15:06:41 +0200

    Program with an Error A

---
+++ ProgWithErrorsA.java
@@ -0,0 +1,17 @@
+public class ProgWithErrorsA{
+  
+  static final int PRIME = 11.213;
+  static final RATE = 15.6;
+  public void main(String [] arg){
+    
+    int i,x,y,w;
+    x = 7;
+    y = 3;
+    x = x + w;
+    PRIME = x + PRIME;
+    System.out.println(PRIME);
+    wages = rate*36.75;
+    System.out.println("Wages = "+wages);
+  }
+}
+  
