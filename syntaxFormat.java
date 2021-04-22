6a7cbc3ab905ac9a744890dc18233e411d40f625
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-04-22 14:17:39 +0200

    Syntax Format

---
+++ syntaxFormat.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class syntaxFormat{
+  
+  static Scanner console = new Scanner(System.in);
+  
+  public static void main(String [] args){
+    
+    int num;
+    double height;
+    String name;
+    
+    System.out.println("Enter an integer: ");
+    System.out.flush();
+    num = console.nextInt();
+    System.out.println();
+    
+    System.out.println("num: " +num);
+    System.out.println("Enter the first name: ");
+    System.out.flush();
+    name = console.next();
+    System.out.println();
+    
+    System.out.print("Enter the height: ");
+    System.out.flush();
+    height = console.nextDouble();
+    System.out.println();
+                       
+    System.out.println("Name: "+name);
+    System.out.println("Height: " +height);
+  }
+}
+                       
+    
