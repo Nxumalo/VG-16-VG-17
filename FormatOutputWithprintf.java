a50125c8c62dccf0fc203a66d14934dd6a38bf9b
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-04-25 21:54:51 +0200

    Formatting Output With printf

---
+++ FormatOutputWithprintf.java
@@ -0,0 +1,16 @@
+public class FormatOutputWithprintf{
+ public static void main(String [] args){
+ 
+   int num = 763;
+   double x = 658.75;
+   String str = “Java System Program”;
+   
+   System.out.println(“123456789012345678901234567890”);
+   System.out.printf(“%5d%7.2f%15s%n”,x,num,str);
+
+   System.out.printf(“num = “ %5d%n”,num);
+   System.out.printf(“x = %10.2f%n”,x);
+   System.out.printf(“str = %15s%n”,str);
+   System.out.printf(“%10s%7d%n”,”Program No.”,4);
+ }
+}
