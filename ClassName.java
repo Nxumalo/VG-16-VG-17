2c1375e273e98a4d1797790f1958426aad2d1cf7
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-04-28 14:47:29 +0200

    I/O

---
+++ ClassName.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+// Add additional packages as needed.
+
+public class ClassName{
+
+ // Declare the appropriate variables.
+
+ public static void main(String [] args) 
+                    throws FileNotFoundException{
+
+     // Create and associate the stream objects.
+   
+     Scanner inFile = 
+       new Scanner (new FileReader(“a:\\prog.out”);
+    
+     PrintWriter outFile = new PrintWriter(“a:\\prog.out”);
+
+       //Code for data manipulation 
+
+       // Close the file.
+
+     inFile.close();
+     outFile.close();
+  }
+}
+ 
