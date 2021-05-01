fa8eac40f058f5623c801fbed74ec6b9323a96a1
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-05-01 15:37:37 +0200

    Read two Numbers

---
+++ Read2Num.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public static void Read2Num{
+
+ public static void main(String [] args){
+  
+   Scanner inFile = new Scanner (new FileReader(“a:\\ExInput.txt”));
+  
+   int num1,num2;
+
+   num1 = inFile.nextInt();
+   num2 = inFile.nextInt();
+
+   outFile.println(“Sum = “+(num1 + num2));
+
+   outFile.close();
+ }
+}
