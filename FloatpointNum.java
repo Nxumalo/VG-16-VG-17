93f0f0b9ab474016fee2e91a5ee247874cce8e44
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-05-01 16:55:41 +0200

    Floating Point Numbers

---
+++ FloatpointNum.java
@@ -0,0 +1,10 @@
+public class FloatpointNum{
+
+ public static void main(String [] args){
+   
+   System.out.println(“3.0 / 7.0 = “+(3.0/7.0));
+   System.out.println(“2.0 / 7.0 = “+(2.0/7.0));
+   System.out.println(“3.0 / 7.0 + 2.0 / 7.0 + 2.0 / 7.0 = “
+                     +(3.0 / 7.0 + 2.0 / 7.0 + 2.0 / 7.0));
+ }
+}
