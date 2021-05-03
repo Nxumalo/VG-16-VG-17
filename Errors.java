b9295fa1c40e09a65cc6130bce6629131dc98713
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-05-04 01:11:46 +0200

    Errors

---
+++ Errors.java
@@ -0,0 +1,32 @@
+public class Errors{
+
+  public static void main(String [] args){
+
+    int a, b;
+    boolean found;
+
+    System.out.print(“Enter the first integer: “);
+    a = console.nextInt();
+    System.out.println();
+
+    System.out.print(“Enter the second integer: “);
+    b = console.nextInt();
+
+    if ( a > a*b && 10 < b )
+       found = 2 * a>b;
+    else
+       {
+       found = 2 * a < b;
+       if (found){
+           a = 3;
+           c = 15;
+           }
+       if (b){
+          b = 0;
+          a = 1;
+
+        }
+      }
+    }
+  }
+}
