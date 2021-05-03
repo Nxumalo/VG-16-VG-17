632bf057f46de499f54b0a3684f11230528a09ac
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-05-04 01:19:50 +0200

    Mystery

---
+++ Mystery2.java
@@ -0,0 +1,21 @@
+public class Mystery2{
+
+   static final int ONE = 5;
+
+   public static void main (String [] args){
+
+      int x, y, w, z;
+      z = 9;
+
+      if (z > 10){
+          x = 12;
+          y = 5, w = x + y + ONE;
+      }
+      else{
+          x = 12;
+          y = 4, w = x + y + ONE;
+      }
+      System.out.println(“w = “+ w);
+  }
+}
+
