5baf99c9ef8f69b331ebfa329dff188a8a3e700d
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-05-05 21:31:47 +0200

    Float - Point Expression

--- Floatpoint.java
+++ Floatpoint.java
@@ -1,4 +1,4 @@
-public static void floatpoint{
+public static void Floatpoint{
  public static void main(String [] args){
   System.out.println(“5.0 + 3.5 = “ + (5.0 + 3.5));
   System.out.println(“3.0 + 9.4 = “ +(3.0 + 9.4));
