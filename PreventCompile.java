e3858401b1169908b9f707a8180a91e55a23565a
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-05-06 16:42:54 +0200

    Prevent Compile

---
+++ PreventCompile.java
@@ -0,0 +1,19 @@
+public class PreventCompile{
+
+   final int N = 2,137;
+
+   public static void main (String [] args){
+
+      int a, b, c, d;
+
+      a := 3;
+      b = 5;
+      c = c + d;
+      N = a + n;
+      for(i = 3; i <= N; i ++){ 
+         System.out.print(“ “ + I);
+         i = i + 1;
+      }
+      System.out.println();
+   }
+}
