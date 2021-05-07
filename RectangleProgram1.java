90a04facc87e076b446724f945d6ad777d48f2fd
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-05-07 08:56:20 +0200

    Rectangle Program

---
+++ RectangleProgram1.java
@@ -0,0 +1,13 @@
+public class RectangleProgram1 extends JFrame{
+ 
+   public RectangleProgram1(){
+   
+      setTitle(“Area and Perimeter of a Rectangle “);
+      setSize(400,300);
+      setVisible(true);
+      setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+   } 
+   public static void main(String [] args){
+       RectangleProgram1 rectObject = new RectangleProgram1();
+   }
