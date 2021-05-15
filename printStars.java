7eb5ba2e064d6f5019101975193ce73428d78afb
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-05-15 17:08:15 +0200

    Print Stars With for Loop

---
+++ printStars.java
@@ -0,0 +1,10 @@
+public static void printStars(){
+  
+  int stars, lines;
+  
+  for(lines = 1; lines <= 2; lines++){
+    for(stars = 1; stars <= 30; stars++)
+      System.out.print("*");
+    System.out.println();
+  }
+}
