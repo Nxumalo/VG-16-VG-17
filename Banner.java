92df84858ee531fb3256d6d6b057a60528cfb86c
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-05-15 17:04:15 +0200

    Banner

---
+++ Banner.java
@@ -0,0 +1,19 @@
+public class Banner{
+  
+  public static void main(String [] args){
+    
+    printStars();
+    System.out.println("*********** Annual **********");
+    printStars();
+    System.out.println("******** Spring Sale ********");
+    printStars();
+ 
+  }
+  
+  public static void printStars(){
+    
+    System.out.println("******************************");
+    System.out.println("******************************");
+    
+  }
+}
