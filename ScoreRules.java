b6d43ca03e5cf9173215d3c718ad74244d82c20d
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-05-20 20:44:17 +0200

    Score Rules

---
+++ ScoreRules.java
@@ -0,0 +1,17 @@
+public class ScoreRules{
+  
+  static final double rate = 10.50;
+  static int z;
+  static double t;
+  
+  public static void main(String [] args){
+    int num;
+    double x,z;
+    char ch;
+    
+    
+    //... 
+    
+  }
+  
+}
