79753561a227de4007956c0b24e76d28826895ec
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-06-01 20:10:44 +0200

    Print Heading

---
+++ printHeading.java
@@ -0,0 +1,7 @@
+public static void printHeading(printWriter outp){
+  
+  outp.println("Course                 Course Average");
+  outp.println("      ID  0 10 20 30 40 50 60 70 80 90 100");
+  outp.println("      |....|....|....|....|....|....|....|....|....|....|....|....|....|....|....|");
+   
+}
