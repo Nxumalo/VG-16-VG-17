d563c5e3706f137ecf6a4a748a74724129141a72
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-06-04 12:48:59 +0200

    Print Time

---
+++ PrintTime.java
@@ -0,0 +1,16 @@
+public void PrintTime(){
+  
+  if(hr < 10)
+    System.out.print("0");
+  System.out.print(hr + " : ");
+  
+  if(min < 10)
+    System.out.print("0");
+  System.out.print(min + " : ");
+  
+  if(sec < 10)
+    System.out.print("0");
+  System.out.print(sec);
+  
+  
+}
