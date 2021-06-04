b371aae550880bf1e5872a795b18335ada5f5894
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-06-04 13:03:53 +0200

    Clock

---
+++ Clock2.java
@@ -0,0 +1,20 @@
+public Clock2(int hours, int minutes, int seconds){
+  
+  if(0 <= hours && hours < 24)
+    hr = hours;
+  else
+    hr = 0;
+  
+  if(0 <= minutes && minutes < 60)
+    min = minutes;
+  else
+    min = 0;
+  
+  if(0 <= seconds && seconds < 60)
+    sec = seconds;
+  else
+    sec = 0;
+    
+    
+  
+}
