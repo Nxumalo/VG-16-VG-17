ad55e91236e692677e04a59912b174901f5db733
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-06-04 20:16:35 +0200

    Test Program Clock

--- TestProgClock.java
+++ TestProgClock.java
@@ -26,7 +26,38 @@ public class TestProgClock{
     yourClock.setTime(5, 45, 16);
     
     System.out.print("Line 13: After setting the time, yourClock: ");
-    yourClock.printTim
+    yourClock.printTime();
+    System.out.println();
+    
+    if(myClock.equals(yourClock))
+      System.out.println("Line 17: Both the times are equal.");
+    else
+      System.out.println("Line 19: The two times are not equal.");
+    
+    System.out.print("Line 20: Enter the hours, minutes, and seconds: ");
+    
+    hours = console.nextInt();
+    minutes = console.nextInt();
+    seconds = console.nextInt();
+    System.out.println();
+    
+    myClock.setTime(hours, minutes, seconds);
+    System.out.print("Line 26: New time of myClock: ");
+    myClock.printTime();
+    System.out.println();
+    
+    myClock.incrementSecond();
+    
+    System.out.print("Line 30: After incrementing the time by one second, myClock: ");
+    
+    myClock.printTime();
+    System.out.println();
+    
+    yourClock.makeCopy(myClock);
+    System.out.print("Line 34: After copying myClock int yourClock, yourClock: ");
+    
+    yourClock.printTime();
+    System.out.println();
     
   }
 }
