340e8ed4121ef3bb6ca635dcf2b07e57c38a4ff5
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-06-05 12:49:17 +0200

    Selection

---
+++ showSelection.java
@@ -0,0 +1,11 @@
+public static void showSelection(){
+ 
+  System.out.println("*** Welcome to 50 cent's  Candy Shop ***");
+  System.out.println("To select an item, enter ");
+  System.out.println("1 for Candy");
+  System.out.println("2 for Chips");
+  System.out.println("3 for Gum");
+  System.out.println("4 for Cookies");
+  System.out.println("9 to exit");
+  
+}
