1ff26778e72797c6969836af468d33c65b3c4187
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-06-05 11:48:53 +0200

    Test Clock

---
+++ TestClock.java
@@ -0,0 +1,11 @@
+import clockPackage.Clock;
+
+public class TestClock{
+ 
+  public static void main(String [] args){
+   
+    Clock myClock = new Clock(12, 30, 45);
+    
+    System.out.println("myClock: " + myClock);   
+  }
+}
