5d2156b443c5c6d480875d0d6c8c1bea2e20bbdf
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-06-05 11:46:13 +0200

    Test Program Person

--- TestProgPerson.java
+++ TestProgPerson.java
@@ -1,6 +1,20 @@
 public class TestProgPerson{
   
-  public static void main(Stringn 
+  public static void main(String [] args){
+   
+    Person name = new Person();
+    
+    Person emp = new Person("Donald","Jackson");
+    
+    System.out.println("Line 3: name: " +name);
+    
+    name.setName("Ashley","Blair");
+    System.out.println("Line 5: name: " +name);
+    
+    System.out.println("Line 6: emp: " +emp);
+    
+    emp.setName("Sandy", "Smith");
+    System.out.println("Line 8: emp: " +emp);
   
-  
+  }
 }
