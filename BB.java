73f0b9d591890ba15539609c27025541dec2e5bb
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-06-20 10:39:24 +0200

    BB

---
+++ BB.java
@@ -0,0 +1,24 @@
+public class BB{
+ 
+  private int one;
+  private int two;
+  
+  public boolean equal(){
+   
+    return (one == two);
+    
+  }
+  
+  public print (){
+  
+    System.out.println(one + " " + two);
+    
+  }
+  public BB (int a, int b){
+   
+    one = a;
+    two = b;
+    
+  }
+  
+}
