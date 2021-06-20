a09d234eb9f1464a9bf474fbd66b70d03158144d
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-06-20 17:03:50 +0200

    Read Code

---
+++ readCode.java
@@ -0,0 +1,8 @@
+public static void readCode(Scanner inFile, int [] list, int length){
+ 
+  int count;
+  
+  for(count = 0; count < length; length; count++)
+    list[count] = inFile.nextInt();
+  
+}
