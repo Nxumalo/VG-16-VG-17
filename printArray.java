bf209a364cda5be808731185ee89af20c0db7afd
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-06-20 11:26:44 +0200

    Print Array

---
+++ printArray.java
@@ -0,0 +1,8 @@
+public static void printArray(int [] list)
+{
+  int index;
+  for(index = 0; index < list.length; index++)
+    System.out.print(list[index] + " ");
+}
+                          
+  
