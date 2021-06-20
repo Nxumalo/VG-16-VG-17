f824bf6f698805a10e2cba6a895873151a778d72
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-06-20 14:23:55 +0200

    Create TestProgStudentData.java

---
+++ TestProgStudentData.java
@@ -0,0 +1,18 @@
+public class TestProgStudedntData{
+ 
+  public static void main(String [] args){
+   
+    StudentData student1 = new StudentData ("Johnny", "Walker", 89, 78, 95, 63, 94);
+    
+    StudentData student2 = new StudentData("Lindiwe", "Zulu", 92 ,82, 90, 70, 87, 99);
+    
+    
+    System.out.println(student1);
+    System.out.println(student2);
+    
+    
+    
+  }
+  
+  
+}
