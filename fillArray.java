0d7fdb3bebbaaf1afa7ea877b9d8fa0b0fc65f4f
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-06-20 11:31:11 +0200

    Fill  Array

---
+++ fillArray.java
@@ -0,0 +1,8 @@
+// input data to store in array 
+public static void fillArray(int [] list, int noOfElements){
+ 
+  int index;
+  for(index = 0; index < noOfElements; index++)
+    list[index] = console.nextInt();
+  
+}
